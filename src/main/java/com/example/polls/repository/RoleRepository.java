ebf419308d4fcea9bc6673022c6163663278a6af
Pooja Prashar <PoojaPrashar@users.noreply.github.com>
2019-10-04 14:57:19 -0700

    Added the repository package

---
+++ src/main/java/com/example/polls/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.example.polls.repository;
+
+import com.example.polls.Model.Role;
+import com.example.polls.Model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
+
